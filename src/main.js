d75b2fe9e6e007249ad35f3b94cc60e5b034a19f
Jason Tumusiime <jasongeorgetumusiime@gmail.com>
2021-01-07 17:15:04 +0300

    Add prompt

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+import { readFile } from 'fs';
+import { createInterface } from 'readline'
+
+const run = (source) => {
+
+} 
+
+const runFile = (scriptPath) => {
+  readFile(scriptPath, (err, data) => {
+    if (err) throw err;
+    run(data.toString())
+  })
+}
+
+const recursiveEval = function (rl) {
+  rl.question('js-lox >> ', function (line) {
+    if (line == 'exit')
+      return rl.close();
+    run(line)
+    recursiveEval(rl);
+  });
+};
+
+const runPrompt = () => {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  recursiveEval(rl);
+}
+
 export const main = (args) => {
   if(args.length > 2) {
     console.log("Usage: js-lox [script]")
